575bac42ca4db42092e005ce67e65bf4ecd34afb
JiHun <pgh979@gmail.com>
2026-01-20 15:13:17 +0900

    메시지 처리 콜백 함수 추가

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <windows.h>
 
+// 각종 메시지를 처리할 함수
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		// Signal that the app should quit
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	return 0;
