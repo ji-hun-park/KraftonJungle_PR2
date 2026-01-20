9466db334b57827df2ab7a357a8449bff559ded7
JiHun <pgh979@gmail.com>
2026-01-20 15:27:03 +0900

    라이브러리와 헤더 파일 추가

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <windows.h>
 
+// D3D 사용에 필요한 라이브러리들을 링크합니다.
+#pragma comment(lib, "user32")
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "d3dcompiler")
+
+// D3D 사용에 필요한 헤더파일들을 포함합니다.
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
 // 각종 메시지를 처리할 함수
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
