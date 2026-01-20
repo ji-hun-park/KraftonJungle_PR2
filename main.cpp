61f6bd40cd73985edf063d6ff3535b808016864b
JiHun <pgh979@gmail.com>
2026-01-20 15:24:56 +0900

    윈도우 생성

--- main.cpp
+++ main.cpp
@@ -18,5 +18,55 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	// 윈도우 클래스 이름
+	WCHAR WindowClass[] = L"JungleWindowClass";
+
+	// 윈도우 타이틀바에 표시될 이름
+	WCHAR Title[] = L"Game Tech Lab";
+
+	// 각종 메시지를 처리할 함수인 WndProc의 함수 포인터를 WindowClass 구조체에 넣는다.
+	WNDCLASSW wndclass = { 0, WndProc, 0, 0, 0, 0, 0, 0, 0, WindowClass };
+
+	// 윈도우 클래스 등록
+	RegisterClassW(&wndclass);
+
+	// 1024 x 1024 크기의 윈도우 생성
+	HWND hwnd = CreateWindowExW(0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1024, 1024,
+		nullptr, nullptr, hInstance, nullptr);
+	
+	bool bIsExit = false;
+
+	// 각종 생성 코드
+
+	// Main Loop (Quit Message가 들어오기 전까지 아래 Loop를 무한히 실행하게 됨)
+	while (bIsExit == false)
+	{
+		MSG msg;
+
+		// 처리할 메시지가 더 이상 없을때 까지 수행
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// 키 입력 메시지를 번역
+			TranslateMessage(&msg);
+
+			// 메시지를 적절한 윈도우 프로시저에 전달, 메시지가 위에서 등록한 WndProc 으로 전달됨
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				bIsExit = true;
+				break;
+			}
+		}
+
+		////////////////////////////////////////////
+		// 매번 실행되는 코드
+
+		////////////////////////////////////////////
+	}
+
+	// 소멸하는 코드
+
 	return 0;
 }
