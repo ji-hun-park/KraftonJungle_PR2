6e12533ae50483fb20b8aa6953bf033b94dbd0ed
JiHun <pgh979@gmail.com>
2026-01-21 16:22:37 +0900

    imgui impl 관련 파일 추가, 헤더 임포트

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@
 #include <d3d11.h>
 #include <d3dcompiler.h>
 
+// ImGui 관련 헤더파일들을 포함합니다.
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "imGui/imgui_impl_win32.h"
+
 // Define the triangle vertices
 struct FVertexSimple
 {
