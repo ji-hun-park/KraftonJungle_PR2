9e39325397eb80c7e772df56537ef707af566c63
JiHun <pgh979@gmail.com>
2026-01-21 18:00:45 +0900

    렌더러 변수들 순서 변경

--- main.cpp
+++ main.cpp
@@ -39,6 +39,20 @@ public:
 	ID3D11InputLayout* SimpleInputLayout;
 	unsigned int Stride;
 	
+	// Direct3D 11 장치(Device)와 장치 컨텍스트(Device Context) 및 스왑 체인(Swap Chain)을 관리하기 위한 포인터들
+	ID3D11Device* Device = nullptr; // GPU와 통신하기 위한 Direct3D 장치
+	ID3D11DeviceContext* DeviceContext = nullptr; // GPU 명령 실행을 담당하는 컨텍스트
+	IDXGISwapChain* SwapChain = nullptr; // 프레임 버퍼를 교체하는 데 사용되는 스왑 체인
+
+	// 렌더링에 필요한 리소스 및 상태를 관리하기 위한 변수들
+	ID3D11Texture2D* FrameBuffer = nullptr; // 화면 출력용 텍스처
+	ID3D11RenderTargetView* FrameBufferRTV = nullptr; // 텍스처를 렌더 타겟으로 사용하는 뷰
+	ID3D11RasterizerState* RasterizerState = nullptr; // 래스터라이저 상태(컬링, 채우기 모드 등 정의)
+	ID3D11Buffer* ConstantBuffer = nullptr; // 쉐이더에 데이터를 전달하기 위한 상수 버퍼
+
+	FLOAT ClearColor[4] = { 0.025f, 0.025f, 0.025f, 1.0f }; // 화면을 초기화(clear)할 때 사용할 색상 (RGBA)
+	D3D11_VIEWPORT ViewportInfo; // 렌더링 영역을 정의하는 뷰포트 정보
+
 	void CreateShader()
 	{
 		ID3DBlob* vertexshaderCSO;
@@ -127,21 +141,6 @@ public:
 			DeviceContext->Unmap(ConstantBuffer, 0);
 		}
 	}
-
-public:
-	// Direct3D 11 장치(Device)와 장치 컨텍스트(Device Context) 및 스왑 체인(Swap Chain)을 관리하기 위한 포인터들
-	ID3D11Device* Device = nullptr; // GPU와 통신하기 위한 Direct3D 장치
-	ID3D11DeviceContext* DeviceContext = nullptr; // GPU 명령 실행을 담당하는 컨텍스트
-	IDXGISwapChain* SwapChain = nullptr; // 프레임 버퍼를 교체하는 데 사용되는 스왑 체인
-
-	// 렌더링에 필요한 리소스 및 상태를 관리하기 위한 변수들
-	ID3D11Texture2D* FrameBuffer = nullptr; // 화면 출력용 텍스처
-	ID3D11RenderTargetView* FrameBufferRTV = nullptr; // 텍스처를 렌더 타겟으로 사용하는 뷰
-	ID3D11RasterizerState* RasterizerState = nullptr; // 래스터라이저 상태(컬링, 채우기 모드 등 정의)
-	ID3D11Buffer* ConstantBuffer = nullptr; // 쉐이더에 데이터를 전달하기 위한 상수 버퍼
-
-	FLOAT ClearColor[4] = { 0.025f, 0.025f, 0.025f, 1.0f }; // 화면을 초기화(clear)할 때 사용할 색상 (RGBA)
-	D3D11_VIEWPORT ViewportInfo; // 렌더링 영역을 정의하는 뷰포트 정보
 
 public:
 	// 렌더러 초기화 함수
